e25b7f798f131522cfe4482a174f6e9932a0b72b
Oleg <oleg17.k@gmail.com>
2023-03-25 21:41:25 +0200

    addded modal1

--- script.js
+++ script.js
@@ -4,3 +4,33 @@ menuBtn.addEventListener("click", function () {
   menuBtn.classList.toggle("active");
   menu.classList.toggle("active");
 });
+
+let closeBtn = document.querySelector(".btn-close");
+let priceGym = document.querySelector('.price-gym');
+let modal1 = document.querySelector('.modal1');
+
+
+function openModal1() {
+  modal1.classList.add("show");
+  modal1.classList.remove("hide");
+}
+
+function closeModal1() {
+  modal1.classList.add("hide");
+  modal1.classList.remove("show");
+}
+
+closeBtn.addEventListener("click", function (e) {
+  closeModal1()
+});
+
+modal1.addEventListener("click", function (e) {
+  if (e.target === modal1) {
+    closeModal1();
+  }
+});
+
+priceGym.addEventListener('click', function () {
+  openModal1();
+})
+
