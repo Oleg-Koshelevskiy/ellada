e6aeba850306554ea18e6cf764d3cfb4c9dea8a7
Oleg <oleg17.k@gmail.com>
2023-03-30 21:59:51 +0300

    added scroll to nav

--- script.js
+++ script.js
@@ -5,38 +5,69 @@ menuBtn.addEventListener("click", function () {
   menu.classList.toggle("active");
 });
 
-let modal = document.querySelectorAll('.modal');
-let openModalBtn = document.querySelectorAll('.openModal')
+let modal = document.querySelectorAll(".modal");
+let openModalBtn = document.querySelectorAll(".openModal");
 let closeModalBtn = document.querySelectorAll(".btn-close");
 
-let info = document.querySelectorAll('.info');
-let trainerContacts = document.querySelectorAll('.trainer-contacts');
+let info = document.querySelectorAll(".info");
+let trainerContacts = document.querySelectorAll(".trainer-contacts");
 
+openModalBtn.forEach((item, index) =>
+  item.addEventListener("click", function (e) {
+    const match = [...modal].filter((el, i) => i === index);
+    match[0].classList.add("show");
+    match[0].classList.remove("hide");
+  })
+);
 
-openModalBtn.forEach((item, index) => item.addEventListener('click', function (e){  
-  const match = [...modal].filter((el, i) => i === index)  
-  match[0].classList.add("show");
-  match[0].classList.remove("hide");
-}));
+closeModalBtn.forEach((item, index) =>
+  item.addEventListener("click", function (e) {
+    const match = [...modal].filter((el, i) => i === index);
+    match[0].classList.add("hide");
+    match[0].classList.remove("show");
+  })
+);
 
-closeModalBtn.forEach((item, index) => item.addEventListener('click', function (e){  
-  const match = [...modal].filter((el, i) => i === index)  
-  match[0].classList.add("hide");
-  match[0].classList.remove("show");
-}));
+modal.forEach((item) =>
+  item.addEventListener("click", function (e) {
+    console.log(e.target, item);
+    if (e.target === item) {
+      item.classList.add("hide");
+      item.classList.remove("show");
+    }
+  })
+);
 
-modal.forEach((item) => item.addEventListener('click', function (e){
-  console.log(e.target, item)  
-  if (e.target === item) {
+info.forEach((item, index) =>
+  item.addEventListener("click", function (e) {
     item.classList.add("hide");
     item.classList.remove("show");
-  }
-}));
+    const match = [...trainerContacts].filter((el, i) => i === index);
+    match[0].classList.add("show");
+    match[0].classList.remove("hide");
+  })
+);
 
-info.forEach((item, index) => item.addEventListener('click', function (e){ 
-  item.classList.add("hide");
-  item.classList.remove("show"); 
-  const match = [...trainerContacts].filter((el, i) => i === index);   
-  match[0].classList.add("show");
-  match[0].classList.remove("hide");
-}));
+const btnScrollToAbout = document.querySelector("#to-about-us");
+const sectionAbout = document.querySelector("#about-us");
+btnScrollToAbout.addEventListener("click", function (e) {
+  sectionAbout.scrollIntoView({ behavior: "smooth" });
+});
+
+const btnScrollToServises = document.querySelector("#to-servises");
+const sectionServises = document.querySelector("#servises");
+btnScrollToServises.addEventListener("click", function (e) {
+  sectionServises.scrollIntoView({ behavior: "smooth" });
+});
+
+const btnScrollToTrainers = document.querySelector("#to-trainers");
+const sectionTrainers = document.querySelector("#trainers");
+btnScrollToTrainers.addEventListener("click", function (e) {
+  sectionTrainers.scrollIntoView({ behavior: "smooth" });
+});
+
+const btnScrollToContacts = document.querySelector("#to-contacts");
+const sectionContacts = document.querySelector("#contacts");
+btnScrollToContacts.addEventListener("click", function (e) {
+  sectionContacts.scrollIntoView({ behavior: "smooth" });
+});
